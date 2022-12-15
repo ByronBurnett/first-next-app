c4d55e97771ea72b27aca959035f87725bff7457
Byron Burnett <76644424+ByronBurnett@users.noreply.github.com>
2022-12-15 19:42:25 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+    
+    
+    
+    return ( 
+       <> 
+      <Head>
+      <title>Ninja List | About</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
+
+          <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem vero dignissimos, asperiores iure enim nobis possimus incidunt consequuntur, voluptate consequatur id molestias sit accusamus labore, officia delectus at assumenda cumque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem vero dignissimos, asperiores iure enim nobis possimus incidunt consequuntur, voluptate consequatur id molestias sit accusamus labore, officia delectus at assumenda cumque!</p>
+          </div>
+          </>
+     );
+}
+ 
+export default About;
